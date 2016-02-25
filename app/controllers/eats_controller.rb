23b9b3f441273d3f2bb5250bc0810c5c7e2ce6ed
Aaron Manaloto <aaronpomanaloto@gmail.com>
2016-02-25 08:53:36 +0800

    Add debug logs and remove README

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -1,6 +1,10 @@
 class EatsController < ApplicationController
 
   def root
+    puts "params: #{params}"
+
+    return if params[:token] != ENV['SLACK_TOKEN']
+
     action = QueryAnalyzer.new.analyze(params[:text])
     response = ::HandlerFactory.new.build(action).new.process params
 
