507336dee54376a9e75c5fd22a79fab2b61db9f2
Aaron Manaloto <aaronpomanaloto@gmail.com>
2016-02-25 09:01:15 +0800

    Filter trigger word

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -1,16 +1,24 @@
 class EatsController < ApplicationController
 
   def root
-    puts "params: #{params}"
+    puts params
 
     return if params[:token] != ENV['SLACK_TOKEN']
 
-    action = QueryAnalyzer.new.analyze(params[:text])
+    action = QueryAnalyzer.new.analyze(pure_text(params))
     response = ::HandlerFactory.new.build(action).new.process params
 
     if response
       render :json => {:text => response}
     end
+  end
+
+
+
+  private
+
+  def pure_text params
+    params[:text].sub params[:trigger_word], ''
   end
 
 end
